132431361c38455fa5d6ed6f481112e5e9a49fef
Fukuo Kadota <app.fk91@gmail.com>
2014-10-11 04:41:04 +0900

    複数コードブロックがあっても大丈夫！

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 module.exports = {
 	book: { assets: "./book", css: [ "block.css" ] },
 	hooks: {
+		// Qiita記法を独自フォーマットに変換
+		"page:before": function(page) {
+			var regixAll = /([^\S]```.+)(:(.+))[\s\S]+?```/g;
+			var regix = /([^\S]```.+)(:(.+))[\s\S]+?```/;
+			var matchAll = page.content.match(regixAll);
+
+			for(var i = 0; matchAll && i < matchAll.length; i++){
+				var match = matchAll[i].match(regix);
+				var input = match.input.replace(match[2], "");
+				var replace = input.replace(match[1], "!FILENAME " + match[3] + match[1]);
+				page.content = page.content.replace(match.input, replace);
+			}
+
+			return page;
+		},
+
+		// 独自フォーマットからfilenameタグを生成
 		"page": function(page) {
+			var regixAll = /(<p>!FILENAME\s([^\s]+)<\/p>)\n<pre>[\s\S]*?(<\/pre>)/g;
 			var regix = /(<p>!FILENAME\s([^\s]+)<\/p>)\n<pre>[\s\S]*?(<\/pre>)/;
 			var replacer = function(filename){
 				return '<div class="code-filename">' + filename + '</div>'; 
@@ -9,11 +27,13 @@ module.exports = {
 
 			for(var i = 0; i < page.sections.length; i++){
 				var section = page.sections[i];
-				var tag = section.content.match(regix);
+				var tagAll = section.content.match(regixAll);
 
-				if(tag){
-					section.content = section.content.replace(tag[1], replacer(tag[2]));
-				}	
+				for(var j = 0; tagAll && j < tagAll.length; j++){
+					var tag = section.content.match(regix);
+					var replace = tag[0].replace(tag[1], replacer(tag[2]));
+					section.content = section.content.replace(tag[0], replace);
+				}
 			}
 
 			return page;
